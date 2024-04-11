06f528b75ddef673a9c8355cf3417a7d42ce5e73
sstieff <144176441+sstieff@users.noreply.github.com>
2024-04-11 18:15:16 +0000

    Adds Junja templates

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'We updated this, again!'
+    title = "Home"
+    return render_template("index.html",title=title)
 
 
 app.run(host='0.0.0.0', port=81)
