66f83fd1a1f2bac239af99dbc1bdfe4e10ba0c34
sstieff <144176441+sstieff@users.noreply.github.com>
2024-04-12 01:23:16 +0000

    Adds About, Contact and Team pages

--- main.py
+++ main.py
@@ -8,5 +8,19 @@ def index():
     title = "Home"
     return render_template("index.html",title=title)
 
+@app.route('/about')
+def about():
+    title = "About"
+    return render_template("about.html",title=title)
+
+@app.route('/team')
+def team():
+    title = "Team"
+    return render_template("team.html",title=title)
+
+@app.route('/contact')
+def contact():
+    title = "Contact"
+    return render_template("contact.html",title=title)
 
 app.run(host='0.0.0.0', port=81)
