1c73df48f25a39f87e7aa980a87a91be328c41a8
sstieff <144176441+sstieff@users.noreply.github.com>
2024-04-29 21:49:50 +0000

    Add stars

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import utils as util
+
 app = Flask(__name__)
 
 
@@ -22,5 +24,21 @@ def team():
 def contact():
     title = "Contact"
     return render_template("contact.html",title=title)
+
+movie_dict = [
+              {"title": "Dune", "genre": "Sci-Fi", "rating":2.5},
+              {"title": "Alien", "genre": "Sci-Fi", "rating":3.5},
+              {"title": "Batman", "genre": "Comics", "rating":4.5}
+             ]
+
+movie_dict = util.movie_stars(movie_dict)
+
+@app.route('/movies')
+def movies():
+  context = {
+    "title": "Movies",
+    "movies": movie_dict
+  }  
+  return render_template("movies.html",**context)
 
 app.run(host='0.0.0.0', port=81)
---
+++ utils.py
@@ -0,0 +1,15 @@
+def movie_stars(movie_dict):
+  for movie in movie_dict:
+    movie['stars'] = add_stars(movie['rating'])
+  return movie_dict
+
+def add_stars(rating):
+  my_return = ""
+  for x in range(10):
+    x += 1
+    if x % 2 == 0 and rating * 2 >= x:
+      my_return += "<span class=\"fa fa-star checked\"></span>" 
+    elif x % 2 == 1 and rating * 2 == x:
+      my_return += "<span class=\"fa fa-star-half checked\"></span> "
+    else: ""
+  return my_return
